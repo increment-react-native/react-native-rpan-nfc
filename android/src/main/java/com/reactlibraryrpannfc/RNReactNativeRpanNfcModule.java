c6d330cadc16072a4e16f3077dab0ef7f73f41e7
kennck <kennettecanales@gmail.com>
2020-10-21 16:59:32 +0800

    udpate

--- android/src/main/java/com/reactlibraryrpannfc/RNReactNativeRpanNfcModule.java
+++ android/src/main/java/com/reactlibraryrpannfc/RNReactNativeRpanNfcModule.java
@@ -1,10 +1,11 @@
 
-package com.reactlibrary;
+package com.reactlibraryrpannfc;
+
+import android.content.Context;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class RNReactNativeRpanNfcModule extends ReactContextBaseJavaModule {
 
@@ -13,6 +14,12 @@ public class RNReactNativeRpanNfcModule extends ReactContextBaseJavaModule {
   public RNReactNativeRpanNfcModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+  }
+
+  @ReactMethod
+  public void show(String text) {
+    Context context = getReactApplicationContext();
+    Toast.makeText(context, text, Toast.LENGTH_LONG).show();
   }
 
   @Override
