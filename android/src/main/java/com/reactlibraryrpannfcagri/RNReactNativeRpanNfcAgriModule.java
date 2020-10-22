86a588b86711366c67847a0dd8762b3ee0a4e5e9
kennck <kennettecanales@gmail.com>
2020-10-22 14:20:39 +0800

    update

--- android/src/main/java/com/reactlibraryrpannfcagri/RNReactNativeRpanNfcAgriModule.java
+++ android/src/main/java/com/reactlibraryrpannfcagri/RNReactNativeRpanNfcAgriModule.java
@@ -1,10 +1,23 @@
 
 package com.reactlibraryrpannfcagri;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+
+import com.rfid.api.ADReaderInterface;
+import com.rfid.api.BluetoothCfg;
+import com.rfid.api.GFunction;
+import com.rfid.api.ISO14443AInterface;
+import com.rfid.api.ISO14443ATag;
+import com.rfid.api.ISO15693Interface;
+import com.rfid.api.ISO15693Tag;
+import com.rfid.def.ApiErrDefinition;
+import com.rfid.def.RfidDef;
 
 public class RNReactNativeRpanNfcAgriModule extends ReactContextBaseJavaModule {
 
@@ -18,5 +31,11 @@ public class RNReactNativeRpanNfcAgriModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNReactNativeRpanNfcAgri";
+  }
+
+  @ReactMethod
+  public void show(String text) {
+    Context context = getReactApplicationContext();
+    Toast.makeText(context, text, Toast.LENGTH_LONG).show();
   }
 }
